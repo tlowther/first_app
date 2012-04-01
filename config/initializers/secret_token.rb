6d5949fdf9d5ae568ed280c59522043b62d7e78c
tlowther <proxy_phil@hotmail.com>
2012-04-01 17:40:48 +1000

    Initial commit for RORtute book 3.2, first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3dc23e883a8e57f845cdbc07e65b869f0fc6f578564726d1aafed0ffc17a0dcdf843c49e6c6bf817d80eeb42bc48845aefd20bd3c5418fadad4a4469ae11f9d1'
